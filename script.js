b7317d3bdda3042d4dbea07594e083934500e35f
rhxelj <rcorsino@gmail.com>
2020-10-31 17:16:24 -0300

    remove duplicates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let txtarea = document.querySelector("textarea[name=txtarea]");
 let txtoutput = document.querySelector("textarea[name=output]");
 let btn = document.querySelector("button");
+let counter = document.querySelector(".counter");
 
 const { log } = console;
 log("txtarea ", txtarea);
@@ -11,5 +12,13 @@ btn.addEventListener("click", function () {
   let rawTxt = txtarea.value;
   let regex = /\S+@\S+/g;
   let emails = rawTxt.match(regex);
-  log(emails);
+  let newEmails = [];
+  for (let i = 0; i < emails.length; i++) {
+    if (newEmails.indexOf(emails[i]) == -1) {
+      newEmails.push(emails[i]);
+    }
+  }
+  emails = newEmails;
+  txtoutput.innerText = emails;
+  counter.innerText = "Emails Found " + emails.length;
 });
