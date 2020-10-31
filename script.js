26e3114815d213c5a4c2d639190bc579e482228c
rhxelj <rcorsino@gmail.com>
2020-10-30 22:46:36 -0300

    regex added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Email Extractor application");
+let txtarea = document.querySelector("textarea[name=txtarea]");
+let txtoutput = document.querySelector("textarea[name=output]");
+let btn = document.querySelector("button");
+
+const { log } = console;
+log("txtarea ", txtarea);
+// log("txtoutput ", txtoutput);
+// log("btn ", btn);
+
+btn.addEventListener("click", function () {
+  let rawTxt = txtarea.value;
+  let regex = /\S+@\S+/g;
+  let emails = rawTxt.match(regex);
+  log(emails);
+});
